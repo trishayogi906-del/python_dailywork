44b3c6146df491a43b21e7d88f9bc8eb59439809
trishayogi906.del <trishayogi906@gmail.com>
2025-12-10 12:05:30 -0800

    1 day work

---
+++ operator/opretor1.py
@@ -0,0 +1,17 @@
+ # take two values as user_input and perform arthmetic opreation on these value ?
+a =int(input("enter a number "))
+b =int(input("enter a number "))
+ # addtion 
+print("a+b = ",a+b)
+ # subtraction
+print("a-b = ",a-b)
+ # multiply 
+print("a*b = ",a*b)
+ # divison 
+print("a/b = ",a/b)
+ # modulas 
+print("a%b = ",a%b)
+ # Exponentiation
+print("a**b = ",a**b)
+# floor divison 
+print("a//b = ",a//b)
