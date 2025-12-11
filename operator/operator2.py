cc3ec1996d2482ff4b99fa86de6ea67a704519a0
trishayogi906.del <trishayogi906@gmail.com>
2025-12-10 19:31:25 -0800

    day2 work

---
+++ operator/operator2.py
@@ -0,0 +1,7 @@
+# membership operator
+# find the "hello " in the given string
+string =input("enter a string")
+if "hello" in string:
+    print("Yes hello is present in the string")
+else:
+    print("Yes hello is present in the string")    
