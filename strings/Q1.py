cfe6cde0172fdb27710a7f0e1ec062b102c44273
trishayogi906.del <trishayogi906@gmail.com>
2025-12-14 21:29:59 -0800

    today work

---
+++ strings/Q1.py
@@ -0,0 +1,4 @@
+# 1. Given a string "Hello World", print the first 5 characters using slicing.
+a="hello world"
+print(a[0:])
+
---
+++ strings/Q2.py
@@ -0,0 +1,5 @@
+# 2. Write a program to input a string and print the string without the first and
+# last character.
+s=input("enter a string")
+print(s[1:-1])
+
