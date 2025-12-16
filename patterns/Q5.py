44057fad002f32fd95f633003062739d04385a25
trishayogi906.del <trishayogi906@gmail.com>
2025-12-15 16:58:03 -0800

    today work

---
+++ patterns/Q5.py
@@ -0,0 +1,7 @@
+temp=1
+for i in range(1,4+1):
+    
+    for j in range(i):
+        print(temp,end=" ")
+        temp+=1
+    print()
---
+++ patterns/Q8.py
@@ -0,0 +1,5 @@
+for i in range(4,0,-1):
+    temp=1
+    for j in range(i):
+        print(temp,end=" ")
+        temp+=1
