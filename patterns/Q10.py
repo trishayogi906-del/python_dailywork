6ad7e2690ce8a31a09e729346f0588dfe8d55004
trishayogi906.del <trishayogi906@gmail.com>
2025-12-15 16:59:58 -0800

    today work

---
+++ patterns/Q10.py
@@ -0,0 +1,10 @@
+for i in range(1,5):
+
+    for j in range(i):
+        print("#",end=" ")
+        
+    print()
+       
+for i in range(3,0,-1):    
+    for j in range(i):
+        print("#",end=" ")
--- patterns/Q9.py
+++ patterns/Q9.py
@@ -1,6 +1,8 @@
+temp=1
 for i in range(4,0,-1):
-    temp=1
+
     for j in range(i):
         print(temp,end=" ")
-        temp+=1
-    print()    
+       
+    print()
+    temp+=1
