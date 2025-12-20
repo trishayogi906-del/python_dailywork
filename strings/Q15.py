9a5152a1d389513e8179068812e8a1f290ad2da5
trishayogi906.del <trishayogi906@gmail.com>
2025-12-19 20:18:25 -0800

    today work

---
+++ strings/Q15.py
@@ -0,0 +1,9 @@
+# 15. Given "OpenAIChatGPT", display sliced formats:
+# ○ a. 0 to 7
+# ○ b. 4 to 10
+# ○ c. Whole string in reverse.
+
+a="OpenAIChatGPT"
+print(a[0:7])
+print(a[4:10])
+print(a[::-1])
