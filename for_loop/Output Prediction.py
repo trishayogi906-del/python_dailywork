1a30c9ee50aa48c7c4f46e4fb135500eb9c72fab
trishayogi906.del <trishayogi906@gmail.com>
2025-12-20 16:30:50 -0800

    today work

---
+++ for_loop/Output Prediction.py
@@ -0,0 +1,18 @@
+# Q23.What will be the output of the following code?for i in range(3): print(i)
+# ans =
+# 0
+# 1
+#  2
+
+# Q24 What will be the output of the following code?for i in range(1, 6, 2): print(i)
+# ans = 1
+#       3
+#       5
+
+# Q25. How many times will the following loop execute?for i in range(10, 0, -2): print(i)
+# ans = 10
+        8
+        6
+        4
+        2
+        
