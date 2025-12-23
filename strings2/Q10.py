249d68ef07d6182c52f224e1049e5e2828dbdd1b
trishayogi906.del <trishayogi906@gmail.com>
2025-12-22 20:38:04 -0800

    today work

---
+++ strings2/Q10.py
@@ -0,0 +1,5 @@
+# 10. Concatenate first name and last name and print the full name.
+first_name=input("enter your first name ")
+last_name=input("enter your last name ")
+full_name=first_name+" "+last_name
+print(full_name.capitalize())
