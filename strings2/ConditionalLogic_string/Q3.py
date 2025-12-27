e728f2d6a90f0128af06c23d0fa2db26402e8359
trishayogi906.del <trishayogi906@gmail.com>
2025-12-26 21:17:21 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q3.py
@@ -0,0 +1,6 @@
+# 3. Ask the user to enter an email, then check if it ends with "@gmail.com".
+st=input("enter a string ")
+if st.endswith("@gmail.com"):
+    print("yes")
+else:
+    print("no")    
