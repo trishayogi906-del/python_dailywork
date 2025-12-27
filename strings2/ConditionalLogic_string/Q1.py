768886c9779ac5be7d289835ebb15ddc9a53676e
trishayogi906.del <trishayogi906@gmail.com>
2025-12-26 21:12:00 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q1.py
@@ -0,0 +1,6 @@
+# 1. Check if a string starts with "A". If yes, print "Starts with A", else print "No".
+st=input("enter  a string ")
+if st.startswith("A"):
+    print("start with A")
+else:
+    print("No")
