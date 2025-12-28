a575231dae8aca0ffd13149a3ec39b1408e89d13
trishayogi906.del <trishayogi906@gmail.com>
2025-12-27 20:51:28 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q8.py
@@ -0,0 +1,8 @@
+# 8. Ask the user to enter a password:
+#   ○ If length < 8: print "Weak Password"
+#   ○ Else print "Strong Password"
+password=input("enter a string")
+if len(password)<8:
+    print("weak password")
+else:
+    print("strong password")    
