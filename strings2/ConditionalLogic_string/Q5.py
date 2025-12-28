4895fc8e0d5a4526079f9da6304745bbe9f49773
trishayogi906.del <trishayogi906@gmail.com>
2025-12-27 20:47:57 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q5.py
@@ -0,0 +1,7 @@
+# 5. Write a program: If the string is entirely in uppercase, print "UPPERCASE", else print "Not uppercase".
+st=input("enter a string")
+
+if st == st.upper():
+    print("UPPERCASE")
+else:
+    print("not uppercase")
