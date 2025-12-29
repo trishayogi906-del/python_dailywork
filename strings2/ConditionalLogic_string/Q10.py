510596fa2af95f277fc854b612e4da6b3cf40b06
trishayogi906.del <trishayogi906@gmail.com>
2025-12-28 16:54:40 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q10.py
@@ -0,0 +1,6 @@
+# 10. If a string ends with ".jpg" print "Image file", else print "Not image file".st =input("enter a string")
+if st.endswith(".jpg"):
+    print("image file")
+else:
+    print("not image file")    
+
