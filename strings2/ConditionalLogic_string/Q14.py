3ff018d3661a91e4ee7474164cc90834c34f9f77
trishayogi906.del <trishayogi906@gmail.com>
2025-12-29 15:14:18 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q14.py
@@ -0,0 +1,7 @@
+# 14. If a string contains spaces → print "Sentence"
+#  ○ Else → print "Single Word"
+st=input("enter a string")
+if " " in st:
+    print("sentence")
+else:
+    print("single word")
