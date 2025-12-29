fbc4de5152018a160c39f43fae1cbf09bad2d665
trishayogi906.del <trishayogi906@gmail.com>
2025-12-29 15:15:33 -0800

    today work

---
+++ strings2/ConditionalLogic_string/Q15.py
@@ -0,0 +1,9 @@
+# 15. Take a string and check:
+# ○ If the length is even → print "Even Length"
+# ○ Else → print "Odd Length"
+st=input("enter a string")
+if (len(st)%2==0):
+     print("even length")
+else:
+    print("odd lenght")   
+  
